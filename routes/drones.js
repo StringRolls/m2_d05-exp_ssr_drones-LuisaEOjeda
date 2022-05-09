70cd1c0805075f322f05910f144d35518bb60f20
Luisa Ojeda <luisaojeda@gmail.com>
2022-05-09 09:51:53 +0200

    sister routes sista

--- routes/drones.js
+++ routes/drones.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // require the Drone model here
 const Drones = require('../models/Drone.model')
 
-router.get('/drones', (req, res, next) => {
+router.get('/drones', (req, res) => {
   Drone.find()
   .then((value) => {
     console.log(`${value.length} drones found`)
@@ -14,26 +14,28 @@ router.get('/drones', (req, res, next) => {
   .catch((err) => console.log("there was an error retrieving the list of drones: ", err))
 });
 
-router.get('/drones/create', (req, res, next) => {
+router
+.route('/drones/create')
+.get((req, res) => {
   // Iteration #3: Add a new drone
   res.render("drones/create-form")
-});
-
-router.post('/drones/create', (req, res, next) => {
+})
+.post((req, res) => {
   // Iteration #3: Add a new drone
   Drone.create(req.body)
   .then(() => res.redirect("/drones"))
   .catch(err => res.redirect("/drones/create"))
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+router
+.route('/drones/:id/edit')
+.get((req, res) => {
   // Iteration #4: Update the drone
   const { id } = req.params
   Drone.findById(id)
   .then(drone => res.render("drones/update-form", drone))
   })
-
-router.post('/drones/:id/edit', (req, res, next) => {
+  .post((req, res) => {
   // Iteration #4: Update the drone
   const name = req.body.name;
   const propellers = req.body.propellers;
@@ -42,9 +44,11 @@ router.post('/drones/:id/edit', (req, res, next) => {
   Drone.findOneAndUpdate(req.params.id, {name, propellers, maxSpeed})
   .then (updatedDrone => console.log(`The updated drone is ${updatedDrone}`))
   .then ( () => res.redirect("drones"))
- });
+  .catch (err => console.log("Unable to update the drone: " + err))
+})
 
-router.post('/drones/:id/delete', (req, res, next) => {
+
+router.post('/drones/:id/delete', (req, res) => {
   // Iteration #5: Delete the drone
  Drone.deleteOne({_id: req.params.id})
  .then(() => res.redirect("/drones"))
